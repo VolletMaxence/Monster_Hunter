0bfe152d8a09435a77dccc0549a181e4dd5343c2
JLussiez <70692644+JLussiez@users.noreply.github.com>
2020-12-29 14:46:06 +0100

    menu

---
+++ site/menu.php
@@ -0,0 +1,10 @@
+<link rel="stylesheet" href="CssMenuJeu">
+<div id="Header">
+        <h1><b>Projet Monster Hunter</b></h1>
+        <li id="menu">
+            <ul><!--Menu-->
+                <li><a href="mhw.php">Monster Hunter World</a></li>
+                <li><a href="mhr.php">Monster Hunter Rise</a></li>
+            </ul>
+        </li>
+    </div>
